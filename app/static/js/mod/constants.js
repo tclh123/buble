d50f11b11fe9c45d4e3eda61527c90398ab17716
tclh123 <tclh123@gmail.com>
2013-10-07 20:00:59 +0800

    fix window size

---
+++ app/static/js/mod/constants.js
@@ -0,0 +1,8 @@
+define(function() {
+    return {
+        app: {
+            fps: 36,
+            screenSize: [window.innerWidth-20, window.innerHeight-35]
+        },
+    };
+});
--- app/static/js/mod/main.js
+++ app/static/js/mod/main.js
@@ -1,6 +1,9 @@
-define(['gamejs'], function(gamejs) {
+define([
+    'gamejs',
+    'mod/constants',
+],function(gamejs, constants) {
     return function() {
-        var display = gamejs.display.setMode([600, 400]);
+        var display = gamejs.display.setMode(constants.app.screenSize);
         display.blit(
             (new gamejs.font.Font('30px Sans-serif')).render('Hello World')
         );
